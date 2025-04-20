ea874c3cf84fb1757edd112293666db2d5a0b940
Cristian-Marcano <cristianmm.1112@gmail.com>
2025-04-20 17:25:40 -0400

    feat(client test)

---
+++ client/script.js
@@ -0,0 +1,8 @@
+"use strict";
+
+const form = globalThis.document.getElementById('form');
+const input = globalThis.document.getElementById('input');
+
+form.addEventListener("submit", (e)=> {
+    e.preventDefault()
+});
