84a7c5e0108efaff9f35645741fef74390fc1839
Cristian-Marcano <cristianmm.1112@gmail.com>
2025-04-23 00:40:38 -0400

    feat(cors)

---
+++ src/middlewares/cors.ts
@@ -0,0 +1,18 @@
+import cors from 'cors'
+
+const ACCETEP_ORIGINS = [
+    "http://localhost:5173",
+    "http://http://127.0.0.1:5500"
+]
+
+export const corsMiddleware = (acceptedOrigins = ACCETEP_ORIGINS) => cors({
+    origin: (requestOrigin, callback) => {
+        if(!requestOrigin) return callback(null, true)
+        
+        if(acceptedOrigins.includes(requestOrigin)) {
+            return callback(null, true)
+        }
+
+        return callback(new Error('Not allowed by CORS'))
+    }
+})
