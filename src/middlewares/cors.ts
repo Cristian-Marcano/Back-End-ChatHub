1cd50073d2fc5d886d99099cd63ff5bcbbad05b0
Cristian-Marcano <cristianmm.1112@gmail.com>
2025-04-25 17:19:32 -0400

    chore(more links)

--- src/middlewares/cors.ts
+++ src/middlewares/cors.ts
@@ -2,7 +2,8 @@ import cors from 'cors'
 
 const ACCETEP_ORIGINS = [
     "http://localhost:5173",
-    "http://http://127.0.0.1:5500"
+    "http://http://127.0.0.1:5500",
+    "http://localhost:3001"
 ]
 
 export const corsMiddleware = (acceptedOrigins = ACCETEP_ORIGINS) => cors({
