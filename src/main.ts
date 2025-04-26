a2d9eea59ca8ebba976ed49d706c3dff43f9a457
Cristian-Marcano <cristianmm.1112@gmail.com>
2025-04-26 16:41:13 -0400

    feat(createApp)

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,30 @@
-const word:string = "Hello World"
+import express, { json } from 'express'
+import { createServer } from 'node:http'
+import { join } from 'node:path'
+import morgan from 'morgan'
+import { corsMiddleware } from './middlewares/cors'
+import { createAuthRouter } from './routes/authRoutes'
+import { userModel } from './models/mysql/userModel'
+import { tempEmailsModel } from './models/mysql/tempEmailsModel'
+import ip from './middlewares/internalIP'
 
-console.log(word)
+
+export function createApp():void {
+    const app = express()
+
+    const PORT = process.env.PORT ?? 3001
+
+    app.use(json())
+    app.use(morgan('dev'))
+    app.use(corsMiddleware())
+    app.use(express.static(join(process.cwd(), '/client')))
+    app.disable('x-powered-by')
+
+    app.use('/', createAuthRouter({userModel, tempEmailsModel}))
+    
+    const server = createServer(app)
+
+    server.listen(PORT, ()=> console.log(`\nServer listen on Port:\n\n\tLocal:   http://localhost:${PORT}\n\n\tNetwork: http://${ip}:${PORT}\n`))
+}
+
+createApp()
