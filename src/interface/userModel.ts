9305adee8d4dda994af68492e1da4e27b7a12360
Cristian-Marcano <cristianmm.1112@gmail.com>
2025-05-06 18:31:56 -0400

    feat(conn in updateUser for transaction)

--- src/interface/userModel.ts
+++ src/interface/userModel.ts
@@ -1,4 +1,5 @@
-import { UUID } from "crypto"
+import { UUID } from "node:crypto"
+import { PoolConnection } from "mysql2/promise"
 import { UserSchema, UserPartialSchema } from "../schemas/userSchemas"
 
 export interface User {
@@ -12,13 +13,13 @@ export interface User {
 export interface IUserModel {
     getUser(params: {input: UserSchema}): Promise<User[]>
 
-    getAllUsers(params: {input: UserSchema, id: string}): Promise<User[]>
+    getAllUsers(params: {input: UserSchema, id: UUID}): Promise<User[]>
 
-    getUserById(params: {id: string}): Promise<User[]>
+    getUserById(params: {id: UUID}): Promise<User[]>
 
     getUserByUsernameOrEmail(params: {input: UserPartialSchema}): Promise<User[]>
 
     createUser(params: {input: UserSchema}): Promise<void>
 
-    updateUser(params: {input: User, id:UUID}): Promise<void>
+    updateUser(params: {input: User, id:UUID}, conn?: PoolConnection): Promise<void>
 }
