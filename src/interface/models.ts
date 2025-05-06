f1acab917480db0185901f9213912cdccaf390ed
Cristian-Marcano <cristianmm.1112@gmail.com>
2025-05-06 18:34:22 -0400

    feat(added IUserInfoModel)

--- src/interface/models.ts
+++ src/interface/models.ts
@@ -1,7 +1,9 @@
 import { IUserModel } from "./userModel"
 import { ITempEmailsModel } from "./tempEmailsModel"
+import { IUserInfoModel } from "./userInfoModel"
 
 export interface IModels {
     userModel?: IUserModel,
+    userInfoModel?: IUserInfoModel,
     tempEmailsModel?: ITempEmailsModel
 }
