d32016980a4ca0e79b5f166758a7e1e4edbdf9a9
Cristian-Marcano <cristianmm.1112@gmail.com>
2025-05-07 22:52:59 -0400

    feat(userEvents)

---
+++ src/socket/userEvents.ts
@@ -0,0 +1,22 @@
+import { Server, Socket } from "socket.io"
+import { IModels } from "../interface/models"
+import { UserService } from "../services/userService"
+import { UserController } from "../controllers/userController"
+
+export function userEventsHandler(namespace:string, io: Server, socket: Socket, {userModel, userInfoModel}: IModels) {
+    
+    const userService = new UserService({userModel, userInfoModel})
+    const userController = new UserController({userService})
+
+    socket.on(`${namespace}:search`, (data) => {
+        userController.search(namespace, io, socket, data)
+    })
+
+    socket.on(`${namespace}:update`, (data) => {
+
+    })
+
+    socket.on(`${namespace}:create-info`, (data) => {
+
+    })
+}
