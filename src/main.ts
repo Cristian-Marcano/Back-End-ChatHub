3c73cb1592be2d78c2d86d2a5dae4f1d602d776b
Cristian-Marcano <cristianmm.1112@gmail.com>
2025-05-07 22:55:14 -0400

    feat(added authMiddlaware & socketEventHandler)

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import express, { json } from 'express'
 import { createServer } from 'node:http'
+import { Server } from 'socket.io'
 import { join } from 'node:path'
 import morgan from 'morgan'
 import { corsMiddleware } from './middlewares/cors'
+import { authMiddleware } from './middlewares/auth'
 import { createAuthRouter } from './routes/authRoutes'
+import { socketEventHandler } from './socket'
 import { userModel } from './models/mysql/userModel'
+import { userInfoModel } from './models/mysql/userInfoModel'
 import { tempEmailsModel } from './models/mysql/tempEmailsModel'
 import ip from './middlewares/internalIP'
 
@@ -23,6 +27,14 @@ export function createApp():void {
     app.use('/', createAuthRouter({userModel, tempEmailsModel}))
     
     const server = createServer(app)
+
+    const io = new Server(server)
+    io.use(authMiddleware)
+
+    io.on('connection', (socket) => {
+        console.log('New connection for client -> ', socket.data)
+        socketEventHandler(io, socket, {userModel, userInfoModel})
+    })
 
     server.listen(PORT, ()=> console.log(`\nServer listen on Port:\n\n\tLocal:   http://localhost:${PORT}\n\n\tNetwork: http://${ip}:${PORT}\n`))
 }
