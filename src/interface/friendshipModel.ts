059dc2e38bd501c555df3083460c705ca170b0d9
Cristian-Marcano <cristianmm.1112@gmail.com>
2025-05-12 21:19:43 -0400

    feat(friendshipModel)

---
+++ src/interface/friendshipModel.ts
@@ -0,0 +1,43 @@
+import { UUID } from "node:crypto"
+import { PoolConnection } from "mysql2/promise"
+import { FriendshipShema, State } from "../schemas/friendshipSchemas"
+import { User } from "./userModel"
+
+export interface Friendship {
+    id: number,
+    primary_user_id: UUID,
+    secondary_user_id: UUID,
+    state: string,
+    create_at: Date,
+    update_at: Date|null
+}
+
+type UserFriend = Pick<User, "username" | "email" | "create_at">
+
+type PrimaryUserFriend = {
+    [K in keyof UserFriend as `primary_user_${string & K}`]: UserFriend[K]
+}
+
+type SecondaryUserFriend = {
+    [K in keyof UserFriend as `secondary_user_${string & K}`]: UserFriend[K]
+}
+
+export type FriendshipUser = UserFriend & Friendship & {
+    user_create_at: Date
+}
+
+export type FriendshipGeneral = PrimaryUserFriend & SecondaryUserFriend & Friendship
+
+export interface IFriendshipModel {
+    getFriendshipById(params: {id: number}): Promise<FriendshipGeneral[]>
+
+    getFriendshipsByPrimaryUserId(params: {id: UUID}): Promise<FriendshipUser[]>
+
+    getFriendshipsBySecondaryUserId(params: {id: UUID}): Promise<FriendshipUser[]>
+
+    createFriendship(params: {input: FriendshipShema}, conn?: PoolConnection): Promise<void>
+
+    updateFriendship(params: {input: State, id: number}, conn?: PoolConnection): Promise<void>
+
+    removeFriendship(params: {id: number}): Promise<void>
+}
---
+++ src/models/mysql/friendshipModel.ts
@@ -0,0 +1,50 @@
+import { UUID } from "node:crypto"
+import { PoolConnection, QueryResult } from "mysql2/promise"
+import { FriendshipGeneral, FriendshipUser, IFriendshipModel } from "../../interface/friendshipModel"
+import { FriendshipShema, State } from "../../schemas/friendshipSchemas"
+import pool from "../../db/mysql"
+
+
+class FriendshipModel implements IFriendshipModel {
+    async getFriendshipById({id}: {id: number}): Promise<FriendshipGeneral[]> {
+        const sql = `SELECT f.id AS id, BIN_TO_UUID(f.primary_user_id) AS primary_user_id, BIN_TO_UUID(f.secondary_user_id) AS secondary_user_id, state, f.create_at AS create_at,
+                    update_at, ua1.username AS primary_user_username, ua1.email AS primary_user_email, ua1.create_at AS primary_user_create_at,
+                    ua2.username AS secondary_user_username,ua2.email AS secondary_user_email, ua2.create_at AS secondary_user_create_at 
+                    FROM friendship AS f JOIN user_account AS ua1 ON f.primary_user_id = ua1.id JOIN user_account AS ua2 ON f.secondary_user_id = ua2.id WHERE f.id = ?`
+        const [friendship] = await pool.query(sql, [id])as QueryResult as [FriendshipGeneral[]]
+        return friendship
+    }
+
+    async getFriendshipsByPrimaryUserId({id}: {id: UUID}): Promise<FriendshipUser[]> {
+        const sql = `SELECT f.id AS id, BIN_TO_UUID(f.primary_user_id) AS primary_user_id, BIN_TO_UUID(f.secondary_user_id) AS secondary_user_id, state, f.create_at AS create_at,
+                    update_at, ua.username AS username, ua.email AS email, ua.create_at AS user_create_at FROM friendship AS f JOIN user_account AS ua ON
+                    f.secondary_user_id = ua.id WHERE primary_user_id = UUID_TO_BIN(?)`
+        const [friendship] = await pool.query(sql, [id]) as QueryResult as [FriendshipUser[]]
+        return friendship
+    }
+
+    async getFriendshipsBySecondaryUserId({id}: {id: UUID}): Promise<FriendshipUser[]> {
+        const sql = `SELECT f.id AS id, BIN_TO_UUID(f.primary_user_id) AS primary_user_id, BIN_TO_UUID(f.secondary_user_id) AS secondary_user_id, state, f.create_at AS create_at,
+                    update_at, ua.username AS username, ua.email AS email, ua.create_at AS user_create_at FROM friendship AS f JOIN user_account AS ua ON
+                    f.primary_user_id = ua.id WHERE secondary_user_id = UUID_TO_BIN(?)`
+        const [friendship] = await pool.query(sql, [id]) as QueryResult as [FriendshipUser[]]
+        return friendship
+    }
+
+    async createFriendship({input}: {input: FriendshipShema}, conn?: PoolConnection): Promise<void> {
+        const execute = conn ?? pool, {primary_user_id, secondary_user_id, state} = input
+        await execute.query('INSERT INTO friendship(primary_user_id, secondary_user_id, state) VALUES (UUID_TO_BIN(?), UUID_TO_BIN(?), ?)', 
+                            [primary_user_id, secondary_user_id, state])
+    }
+
+    async updateFriendship({input, id}: {input: State, id: number}, conn?: PoolConnection): Promise<void> {
+        const execute = conn ?? pool
+        await execute.query('UPDATE friendship SET state = ? WHERE id = ?', [input, id])
+    }
+
+    async removeFriendship({id}: {id: number}): Promise<void> {
+        await pool.query('DELETE FROM friendship WHERE id = ?', [id])
+    }
+}
+
+export const friendshipModel = new FriendshipModel()
