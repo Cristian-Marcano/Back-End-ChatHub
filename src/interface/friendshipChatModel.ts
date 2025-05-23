dce9d50bfabc5596d7b40b930570a88561e05125
Cristian-Marcano <cristianmm.1112@gmail.com>
2025-05-23 18:45:05 -0400

    feat(friendshipChatSchemas)

--- src/interface/friendshipChatModel.ts
+++ src/interface/friendshipChatModel.ts
@@ -3,7 +3,7 @@ import { PoolConnection } from "mysql2/promise"
 import { Chat } from "./chatModel"
 import { UserInfo } from "./userInfoModel"
 import { PaginationNameSchema, PaginationSchema } from "../schemas/paginationSchemas"
-import { NicknamesPartialSchemas } from "../schemas/friendshipChatSchemas"
+import { FriendshipChatSchema, NicknamesPartialSchema } from "../schemas/friendshipChatSchemas"
 
 export type FriendshipChat = Pick<Chat, "id" | "create_at" | "nickname" | "photo"> & Pick<UserInfo, "about">
 
@@ -12,9 +12,9 @@ export interface IFriendshipChatModel {
 
     getFriendshipChatsByName(params: {input: PaginationNameSchema, id: UUID}): Promise<FriendshipChat[]>
 
-    createFriendshipChat(params: {idFriendship: number, idChat: number}, conn?: PoolConnection): Promise<void>
+    createFriendshipChat(params: {input: FriendshipChatSchema}, conn?: PoolConnection): Promise<void>
 
-    updateFriendshipChat(params: {input: NicknamesPartialSchemas, id: number}, conn?: PoolConnection): Promise<void>
+    updateFriendshipChat(params: {input: NicknamesPartialSchema, id: number}, conn?: PoolConnection): Promise<void>
 
     removeFriendshipChat(params: {id: number}, conn?: PoolConnection): Promise<void>
 }
--- src/schemas/friendshipChatSchemas.ts
+++ src/schemas/friendshipChatSchemas.ts
@@ -5,11 +5,28 @@ const nickname = z.string({
     required_error: 'Nickname is required'
 }).trim().max(100,'Nickname must be max length 100')
 
+
+const friendshipChatSchemas = z.object({
+    friendshipId: z.number({
+        invalid_type_error: "frienshipId must be a number",
+        required_error: 'friendshipId is required'
+    }).int('friendshipId must be a integer').positive('friendshipId must be positive'),
+    chatId: z.number({
+        invalid_type_error: "chatId must be a number",
+        required_error: 'chatId is required'
+    }).int('chatId must be a integer').positive('chatId must be positive')
+})
+
+
 const nicknamesPartialSchemas = z.object({
     primary_nickname: nickname,
     secondary_nickname: nickname
 }).partial()
 
-export type NicknamesPartialSchemas = z.infer<typeof nicknamesPartialSchemas>
+export type FriendshipChatSchema = z.infer<typeof friendshipChatSchemas>
+
+export type NicknamesPartialSchema = z.infer<typeof nicknamesPartialSchemas>
+
+export const validateFriendshipChat = (input: object) => friendshipChatSchemas.safeParse(input)
 
 export const validatePartialNicknames = (input: object) => nicknamesPartialSchemas.safeParse(input)
