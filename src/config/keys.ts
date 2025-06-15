27b13ef9ffff120a357b3f4ff9ab801fb360832c
Cristian-Marcano <cristianmm.1112@gmail.com>
2025-06-15 15:11:58 -0400

    fix: connection to DB

--- src/config/keys.ts
+++ src/config/keys.ts
@@ -1,6 +1,6 @@
 export const CONFIG_MYSQL = {
-    host: process.env.HOST ?? 'localhost',
-    user: process.env.USER ?? 'root',
+    host: process.env.DB_HOST ?? 'localhost',
+    user: process.env.DB_USER ?? 'root',
     port: Number(process.env.DB_PORT) ?? 3306,
     password: process.env.DB_PASSWORD ?? '',
     database: process.env.DB ?? 'chathub'
